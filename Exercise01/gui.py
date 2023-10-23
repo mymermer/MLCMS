ad31ef0383526cabfe62c85c34e0f1379c132ca8
hammad <hammadbasithb@gmail.com>
2023-10-23 19:48:30 +0200

    Task 2 completed

--- Exercise01/gui.py
+++ Exercise01/gui.py
@@ -1,89 +1,85 @@
 import sys
 import tkinter
-from tkinter import Button, Canvas, Menu
+from tkinter import Button, Canvas, Menu, filedialog
 from scenario_elements import Scenario, Pedestrian
-
+import json
 
 class MainGUI():
-    """
-    Defines a simple graphical user interface.
-    To start, use the `start_gui` method.
-    """
 
-    def create_scenario(self, ):
-        print('create not implemented yet')
+    def __init__(self):
+        self.win = None
+        self.canvas = None
+        self.canvas_image = None
+        self.scenario = None
 
+    def create_scenario(self, scenario_file):
+        with open(scenario_file, 'r') as file:
+            data = json.load(file)
 
-    def restart_scenario(self, ):
-        print('restart not implemented yet')
+        self.scenario = Scenario(data['width'], data['height'])
+        print(self.scenario)
 
+        for target in data['targets']:
+            self.scenario.grid[target[0], target[1]] = Scenario.NAME2ID['TARGET']
 
-    def step_scenario(self, scenario, canvas, canvas_image):
-        """
-        Moves the simulation forward by one step, and visualizes the result.
+        self.scenario.pedestrians = []
 
-        Args:
-            scenario (scenario_elements.Scenario): Add _description_
-            canvas (tkinter.Canvas): Add _description_
-            canvas_image (missing _type_): Add _description_
-        """
-        scenario.update_step()
-        scenario.to_image(canvas, canvas_image)
+        for pedestrian in data['pedestrians']:
+            position = tuple(pedestrian['position'])
+            desired_speed = pedestrian['desiredSpeed']
+            self.scenario.pedestrians.append(Pedestrian(position, desired_speed))
 
+        for obstacle in data['obstacles']:
+            self.scenario.grid[obstacle[0], obstacle[1]] = Scenario.NAME2ID['OBSTACLE']
 
-    def exit_gui(self, ):
-        """
-        Close the GUI.
-        """
+        self.scenario.recompute_target_distances()
+        self.scenario.to_image(self.canvas, self.canvas_image)
+
+    def restart_scenario(self):
+        print('Restart not implemented yet')
+
+    def step_scenario(self):
+        if self.scenario:
+            self.scenario.update_step()
+            self.scenario.to_image(self.canvas, self.canvas_image)
+        else:
+            print('No scenario loaded. Load a scenario before stepping.')
+
+    def exit_gui(self):
         sys.exit()
 
+    def load_scenario(self):
+        scenario_file = filedialog.askopenfilename(
+            title="Select Scenario File", filetypes=(("JSON files", "*.json"), ("all files", "*.*"))
+        )
+        if scenario_file:
+            self.create_scenario(scenario_file)
 
-    def start_gui(self, ):
-        """
-        Creates and shows a simple user interface with a menu and multiple buttons.
-        Only one button works at the moment: "step simulation".
-        Also creates a rudimentary, fixed Scenario instance with three Pedestrian instances and multiple targets.
-        """
-        win = tkinter.Tk()
-        win.geometry('500x500')  # setting the size of the window
-        win.title('Cellular Automata GUI')
+    def start_gui(self):
+        self.win = tkinter.Tk()
+        self.win.geometry('500x500')
+        self.win.title('Cellular Automata GUI')
 
-        menu = Menu(win)
-        win.config(menu=menu)
+
+        menu = Menu(self.win)
+        self.win.config(menu=menu)
         file_menu = Menu(menu)
         menu.add_cascade(label='Simulation', menu=file_menu)
-        file_menu.add_command(label='New', command=self.create_scenario)
+        file_menu.add_command(label='New', command=lambda: self.load_scenario())
         file_menu.add_command(label='Restart', command=self.restart_scenario)
         file_menu.add_command(label='Close', command=self.exit_gui)
 
-        canvas = Canvas(win, width=Scenario.GRID_SIZE[0], height=Scenario.GRID_SIZE[1])  # creating the canvas
-        canvas_image = canvas.create_image(5, 50, image=None, anchor=tkinter.NW)
-        canvas.pack()
+        self.canvas = Canvas(self.win, width=Scenario.GRID_SIZE[0], height=Scenario.GRID_SIZE[1])
+        self.canvas_image = self.canvas.create_image(5, 50, image=None, anchor=tkinter.NW)
+        self.canvas.pack()
+        self.create_scenario('scenario.json')
 
-        sc = Scenario(100, 100)
-
-        sc.grid[23, 25] = Scenario.NAME2ID['TARGET']
-        sc.grid[23, 45] = Scenario.NAME2ID['TARGET']
-        sc.grid[43, 55] = Scenario.NAME2ID['TARGET']
-        sc.recompute_target_distances()
-
-        sc.pedestrians = [
-            Pedestrian((31, 2), 2.3),
-            Pedestrian((1, 10), 2.1),
-            Pedestrian((80, 70), 2.1)
-        ]
-
-        # can be used to show pedestrians and targets
-        sc.to_image(canvas, canvas_image)
-
-        # can be used to show the target grid instead
-        # sc.target_grid_to_image(canvas, canvas_image)
-
-        btn = Button(win, text='Step simulation', command=lambda: self.step_scenario(sc, canvas, canvas_image))
+        btn = Button(self.win, text='Step simulation', command=lambda: self.step_scenario())
         btn.place(x=20, y=10)
-        btn = Button(win, text='Restart simulation', command=self.restart_scenario)
+        btn = Button(self.win, text='Restart simulation', command=self.restart_scenario)
         btn.place(x=200, y=10)
-        btn = Button(win, text='Create simulation', command=self.create_scenario)
+        btn = Button(self.win, text='Create simulation', command=lambda: self.load_scenario())
         btn.place(x=380, y=10)
 
-        win.mainloop()
+        self.win.mainloop()
+
