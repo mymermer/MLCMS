e411d2f6ae4bf96d7ad120789a24435631ed4be8
Hammad <hammad.basit@avelios.com>
2023-10-26 13:41:31 +0200

    Djikstra Algorithm

--- Exercise01/scenario_elements.py
+++ Exercise01/scenario_elements.py
@@ -2,6 +2,8 @@ import scipy.spatial.distance
 from PIL import Image, ImageTk
 import numpy as np
 
+from dijkstra import Dijkstra_algorithm #new file
+
 
 class Pedestrian:
     """
@@ -11,6 +13,7 @@ class Pedestrian:
     def __init__(self, position, desired_speed):
         self._position = position
         self._desired_speed = desired_speed
+        self._starting_position = position
 
     @property
     def position(self):
@@ -33,22 +36,27 @@ class Pedestrian:
             if 0 <= x + self._position[0] < scenario.width and 0 <= y + self._position[1] < scenario.height and np.abs(x) + np.abs(y) > 0
         ]
 
-    def update_step(self, scenario):
+    def update_step(self, scenario: "Scenario"):
         """
-        Moves to the cell with the lowest distance to the target.
+        Moves to the cell by cost.
         This does not take obstacles or other pedestrians into account.
         Pedestrians can occupy the same cell.
 
         :param scenario: The current scenario instance.
         """
         neighbors = self.get_neighbors(scenario)
-        next_cell_distance = scenario.target_distance_grids[self._position[0]][self._position[1]]
         next_pos = self._position
+        print(scenario)
+        next_cell_distance = scenario.dijkstra.estimate_cost(self._position[0], self._position[1])
+
         for (n_x, n_y) in neighbors:
-            if next_cell_distance > scenario.target_distance_grids[n_x, n_y]:
+            if next_cell_distance > scenario.dijkstra.estimate_cost(n_x, n_y):
                 next_pos = (n_x, n_y)
-                next_cell_distance = scenario.target_distance_grids[n_x, n_y]
+                next_cell_distance = scenario.dijkstra.estimate_cost(n_x, n_y)
         self._position = next_pos
+
+    def reset_step(self):
+        self._position = self._starting_position
 
 
 class Scenario:
@@ -86,15 +94,13 @@ class Scenario:
         self.grid_image = None
         self.grid = np.zeros((width, height))
         self.pedestrians = []
-        self.target_distance_grids = self.recompute_target_distances()
 
-    def recompute_target_distances(self):
-        self.target_distance_grids = self.update_target_grid()
-        return self.target_distance_grids
+        #there might be problem in deciding height, width in my coding, however, if grid is square it won't be problem.
+        self.dijkstra= None
 
-    def update_target_grid(self):
+    def update_cost(self):
         """
-        Computes the shortest distance from every grid point to the nearest target cell.
+        Uses dijkstra algorthim to calculate cost of the plain.
         This does not take obstacles into account.
         :returns: The distance for every grid cell, as a np.ndarray.
         """
@@ -106,20 +112,52 @@ class Scenario:
         if len(targets) == 0:
             return np.zeros((self.width, self.height))
 
-        targets = np.row_stack(targets)
-        x_space = np.arange(0, self.width)
-        y_space = np.arange(0, self.height)
-        xx, yy = np.meshgrid(x_space, y_space)
-        positions = np.column_stack([xx.ravel(), yy.ravel()])
+        print(targets)
 
-        # after the target positions and all grid cell positions are stored,
-        # compute the pair-wise distances in one step with scipy.
-        distances = scipy.spatial.distance.cdist(targets, positions)
+        check_for_obstacle = np.zeros((self.width, self.height))
 
-        # now, compute the minimum over all distances to all targets.
-        distances = np.min(distances, axis=0)
+        for x in range(self.width):
+            for y in range(self.height):
+                check_for_obstacle[y][x] = bool(self.grid[x, y] == Scenario.NAME2ID['OBSTACLE'])
 
-        return distances.reshape((self.width, self.height))
+        self.dijkstra = Dijkstra_algorithm(self.height, self.width, targets,
+                                           check_for_obstacle)  # cost regarding to Dijkstra_algorithm
+        self.dijkstra.execute()
+
+    # def recompute_target_distances(self):
+    #     self.target_distance_grids = self.update_target_grid()
+    #     return self.target_distance_grids
+
+
+
+    # def update_target_grid(self):
+    #     """
+    #     Computes the shortest distance from every grid point to the nearest target cell.
+    #     This does not take obstacles into account.
+    #     :returns: The distance for every grid cell, as a np.ndarray.
+    #     """
+    #     targets = []
+    #     for x in range(self.width):
+    #         for y in range(self.height):
+    #             if self.grid[x, y] == Scenario.NAME2ID['TARGET']:
+    #                 targets.append([y, x])  # y and x are flipped because they are in image space.
+    #     if len(targets) == 0:
+    #         return np.zeros((self.width, self.height))
+    #
+    #     targets = np.row_stack(targets)
+    #     x_space = np.arange(0, self.width)
+    #     y_space = np.arange(0, self.height)
+    #     xx, yy = np.meshgrid(x_space, y_space)
+    #     positions = np.column_stack([xx.ravel(), yy.ravel()])
+    #
+    #     # after the target positions and all grid cell positions are stored,
+    #     # compute the pair-wise distances in one step with scipy.
+    #     distances = scipy.spatial.distance.cdist(targets, positions)
+    #
+    #     # now, compute the minimum over all distances to all targets.
+    #     distances = np.min(distances, axis=0)
+    #
+    #     return distances.reshape((self.width, self.height))
 
     def update_step(self):
         """
@@ -134,24 +172,24 @@ class Scenario:
     def cell_to_color(_id):
         return Scenario.NAME2COLOR[Scenario.ID2NAME[_id]]
 
-    def target_grid_to_image(self, canvas, old_image_id):
-        """
-        Creates a colored image based on the distance to the target stored in
-        self.target_distance_gids.
-        :param canvas: the canvas that holds the image.
-        :param old_image_id: the id of the old grid image.
-        """
-        im = Image.new(mode="RGB", size=(self.width, self.height))
-        pix = im.load()
-        for x in range(self.width):
-            for y in range(self.height):
-                target_distance = self.target_distance_grids[x][y]
-                pix[x, y] = (max(0, min(255, int(10 * target_distance) - 0 * 255)),
-                             max(0, min(255, int(10 * target_distance) - 1 * 255)),
-                             max(0, min(255, int(10 * target_distance) - 2 * 255)))
-        im = im.resize(Scenario.GRID_SIZE, Image.NONE)
-        self.grid_image = ImageTk.PhotoImage(im)
-        canvas.itemconfigure(old_image_id, image=self.grid_image)
+    # def target_grid_to_image(self, canvas, old_image_id):
+    #     """
+    #     Creates a colored image based on the distance to the target stored in
+    #     self.target_distance_gids.
+    #     :param canvas: the canvas that holds the image.
+    #     :param old_image_id: the id of the old grid image.
+    #     """
+    #     im = Image.new(mode="RGB", size=(self.width, self.height))
+    #     pix = im.load()
+    #     for x in range(self.width):
+    #         for y in range(self.height):
+    #             target_distance = self.target_distance_grids[x][y]
+    #             pix[x, y] = (max(0, min(255, int(10 * target_distance) - 0 * 255)),
+    #                          max(0, min(255, int(10 * target_distance) - 1 * 255)),
+    #                          max(0, min(255, int(10 * target_distance) - 2 * 255)))
+    #     im = im.resize(Scenario.GRID_SIZE, Image.NONE)
+    #     self.grid_image = ImageTk.PhotoImage(im)
+    #     canvas.itemconfigure(old_image_id, image=self.grid_image)
 
     def to_image(self, canvas, old_image_id):
         """
