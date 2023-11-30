fda88db6f3ec9fd90f49f86d98b4490fa092eb9a
annaaa4701 <annaaa4701@gmail.com>
2023-11-30 16:51:14 +0100

    revisebonus

--- Exercise03/task3/task3_functions.py
+++ Exercise03/task3/task3_functions.py
@@ -9,32 +9,30 @@ import pandas as pd
 def load_mnist_data():
     (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data() # Load MNIST data
     
-    # Normalize pixel values
+    # Normalize pixel values between 0 and 1
     x_train = x_train.astype('float32') / 255.
     x_test = x_test.astype('float32') / 255.
     
-    # Reshape images
+    # Reshape images to a flattened format : 2D array -> 1D array (single vector)
     x_train = x_train.reshape(x_train.shape[0], -1)
     x_test = x_test.reshape(x_test.shape[0], -1)
     
     return (x_train, y_train), (x_test, y_test)
-
-
-data_shape = 784 # shape of the data (28x28=784)
-latent_dim = 2 # dimension of the latent space : to be changed for the last question of task 3
-batch_size = 128 # batch size for training
 
 # Create VAE
 def VAE(data_shape, latent_dim) : 
 
     inputs = tf.keras.Input(shape=data_shape)
 
-        # Encoder
+        # Encoder : outputs the mean and standard deviation of the approximate posterior qφ(z|x) and samples z from it
+
+    # 2 hidden layers with 256 neurons each and ReLU activation function
     x = tf.keras.layers.Dense(256, activation='relu')(inputs)
     x = tf.keras.layers.Dense(256, activation='relu')(x)
-    # Mean and standard deviation outputs for the approximate posterior qφ(z|x)
+
+    # Mean and standard deviation outputs
     z_mean = tf.keras.layers.Dense(latent_dim)(x)
-    z_log_var = tf.keras.layers.Dense(latent_dim)(x) # or tf.keras.layers.Activation('softplus')(z_log_var) -> positivity insurance
+    z_log_var = tf.keras.layers.Dense(latent_dim, activation='softplus')(x) #softplus activation function is used to ensure positivity of the standard deviation
 
     def sampling(args):
         """
@@ -49,17 +47,21 @@ def VAE(data_shape, latent_dim) :
     z = tf.keras.layers.Lambda(sampling)([z_mean, z_log_var])
     encoder = tf.keras.Model(inputs, [z_mean, z_log_var, z], name='encoder')
 
-        # Decoder
+        # Decoder : outputs the mean of the likelihood pθ(x|z) given z
     latent_inputs = tf.keras.Input(shape=(latent_dim,), name='z_sampling')
+
+    # 2 hidden layers with 256 neurons each and ReLU activation function
     x = tf.keras.layers.Dense(256, activation='relu')(latent_inputs)
     x = tf.keras.layers.Dense(256, activation='relu')(x)
-    outputs_mean = tf.keras.layers.Dense(data_shape, activation='sigmoid')(x) #Outputs only the mean of likelihood pθ(x|z)
+
+    outputs_mean = tf.keras.layers.Dense(data_shape, activation='sigmoid')(x) #sigmoid activation function is used to ensure that the output is between 0 and 1
     # Implement standard deviation as a trainable variable of the model
     decoder_stddev = tf.Variable(tf.ones(shape=(data_shape,), dtype=tf.float32), name='decoder_stddev') 
     decoded_with_stddev = tf.keras.layers.Lambda(lambda x: x[0] * x[1])([outputs_mean, decoder_stddev])
     decoder = tf.keras.Model(latent_inputs, decoded_with_stddev, name='decoder')
 
-    outputs = decoder(encoder(inputs)[2]) 
+    outputs = decoder(encoder(inputs)[2]) #  = decoder(z)
+    
         # Define the loss function
     # Reconstruction loss : binary cross-entropy between the input and output
     reconstruction_loss = tf.keras.losses.binary_crossentropy(inputs, outputs)
@@ -78,4 +80,3 @@ def VAE(data_shape, latent_dim) :
     optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
     vae.compile(optimizer=optimizer)
     return vae, encoder, decoder
-
