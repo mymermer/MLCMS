4f30d3eec43a08b3313cede59d14d85241fe9433
Muhammed Yusuf Mermer <myusufmermer@gmail.com>
2024-01-30 10:28:02 +0100

    minor updates for umap

--- Project/UMAP_GPU.py
+++ Project/UMAP_GPU.py
@@ -1,8 +1,11 @@
+#Follow the link for setting up cudf and cuml libraries with wsl2:  https://docs.rapids.ai/install#wsl2-pip
+
 import cupy as cp
 from cuml.manifold import UMAP
 import matplotlib.pyplot as plt
-import gc
 import random
+from sklearn.datasets import make_swiss_roll
+
 
 def umap_visualization(model, subset_size=None, n_neighbors=15, n_components=2):
     # Get all words from the vocabulary
@@ -21,19 +24,43 @@ def umap_visualization(model, subset_size=None, n_neighbors=15, n_components=2):
 
     # Apply UMAP on all words
     umap = UMAP(n_neighbors=n_neighbors, n_components=n_components)
-    transformed_vectors = umap.fit_transform(cp.array(list(word_vectors), dtype=cp.float32))
+    transformed_vectors = umap.fit_transform(cp.asarray(list(word_vectors), dtype=cp.float32))
 
     # Convert the transformed vectors to numpy arrays for plotting
     transformed_vectors = cp.asnumpy(transformed_vectors)
-
-    # Free up memory
-    gc.collect()
 
     # Create a scatter plot with small dots
     plt.figure(figsize=(10, 8))
     plt.scatter(transformed_vectors[:, 0], transformed_vectors[:, 1], marker='.', s=1)
 
     plt.title("UMAP Visualization")
+    plt.xlabel("Embedding Dimension 1")
+    plt.ylabel("Embedding Dimension 2")
+    plt.show()
+
+
+
+
+
+def umap_visualization_swiss(n_samples=1000, n_neighbors=15, n_components=2):
+    # Generate the Swiss roll dataset
+    data, _ = make_swiss_roll(n_samples)
+
+    # Convert the data to a cupy array
+    data_cp = cp.asarray(data, dtype=cp.float32)
+
+    # Apply UMAP
+    umap = UMAP(n_neighbors=n_neighbors, n_components=n_components)
+    transformed_data = umap.fit_transform(data_cp)
+
+    # Convert the transformed data to numpy arrays for plotting
+    transformed_data_np = cp.asnumpy(transformed_data)
+
+    # Create a scatter plot with small dots
+    plt.figure(figsize=(10, 8))
+    plt.scatter(transformed_data_np[:, 0], transformed_data_np[:, 1], marker='.', s=1)
+
+    plt.title("UMAP Visualization of Swiss Roll")
     plt.xlabel("Embedding Dimension 1")
     plt.ylabel("Embedding Dimension 2")
     plt.show()
